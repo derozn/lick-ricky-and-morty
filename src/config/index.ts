05665a0bd8b095e8a81f1459277f1034e4d4ea84
Nick De Rozarieux <banterstudios@gmail.com>
2021-06-20 18:54:47 +0100

    Allow for dynamic port

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const config = {
   RICK_AND_MORTY_CHARACTER_ENDPOINT: `${process.env.RICK_AND_MORTY_ENDPOINT}/character`,
+  PORT: process.env.PORT ?? 3000,
 };
--- src/lib/absoluteRequest/absoluteRequest.ts
+++ src/lib/absoluteRequest/absoluteRequest.ts
@@ -1,13 +1,16 @@
 import absoluteUrl from 'next-absolute-url';
 import { IncomingMessage } from 'http';
 import { request, RequestOptions } from '@/lib/request';
+import { config } from '@/config';
+
+const { PORT } = config;
 
 export const requestWithAbsoluteUrl = <T>(
   req: IncomingMessage,
   relativeUrl: string,
   options: RequestOptions = {},
 ): Promise<T> => {
-  const { protocol, host } = absoluteUrl(req, 'localhost:3000');
+  const { protocol, host } = absoluteUrl(req, `localhost:${PORT}`);
   const url = `${protocol}//${host}${relativeUrl}`;
 
   return request<T>(url, options);
